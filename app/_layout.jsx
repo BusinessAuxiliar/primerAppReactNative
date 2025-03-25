7ec0f203597114de8a6131fcc4ace9bf0c9fbeae
BusinessAuxiliar <business.sistema.auxiliar@gmail.com>
2025-03-25 11:50:10 -0300

    fonts&GlobalProviderCommented

--- app/_layout.jsx
+++ app/_layout.jsx
@@ -1,19 +1,20 @@
 import { Text, View, StyleSheet } from 'react-native';
 import {Slot, Stack} from'expo-router';
 import {useFonts} from 'expo-font'
+import { useEffect } from 'react';
 
 
 function RootLayout () {
 const  {fontsLoaded, error} = useFonts ({
-  "Poppins-Black": require("../assets/fonts/Poppins-Black.ttf"),
-  "Poppins-Bold": require("../assets/fonts/Poppins-Bold.ttf"),
-  "Poppins-ExtraBold": require("../assets/fonts/Poppins-ExtraBold.ttf"),
-  "Poppins-ExtraLight": require("../assets/fonts/Poppins-ExtraLight.ttf"),
-  "Poppins-Light": require("../assets/fonts/Poppins-Light.ttf"),
-  "Poppins-Medium": require("../assets/fonts/Poppins-Medium.ttf"),
-  "Poppins-Regular": require("../assets/fonts/Poppins-Regular.ttf"),
-  "Poppins-SemiBold": require("../assets/fonts/Poppins-SemiBold.ttf"),
-  "Poppins-Thin": require("../assets/fonts/Poppins-Thin.ttf"),
+  "Poppins-Black": require("../Front-end/assets/fonts/Poppins-Black.ttf"),
+  "Poppins-Bold": require("../Front-end/assets/fonts/Poppins-Bold.ttf"),
+  "Poppins-ExtraBold": require("../Front-end/assets/fonts/Poppins-ExtraBold.ttf"),
+  "Poppins-ExtraLight": require("../Front-end/assets/fonts/Poppins-ExtraLight.ttf"),
+  "Poppins-Light": require("../Front-end/assets/fonts/Poppins-Light.ttf"),
+  "Poppins-Medium": require("../Front-end/assets/fonts/Poppins-Medium.ttf"),
+  "Poppins-Regular": require("../Front-end/assets/fonts/Poppins-Regular.ttf"),
+  "Poppins-SemiBold": require("../Front-end/assets/fonts/Poppins-SemiBold.ttf"),
+  "Poppins-Thin": require("../Front-end/assets/fonts/Poppins-Thin.ttf"),
 });
 
 useEffect(() => {
