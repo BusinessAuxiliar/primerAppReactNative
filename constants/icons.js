b820dc0261e79fd4f23f8cabf68e6a11602c17f9
BusinessAuxiliar <business.sistema.auxiliar@gmail.com>
2025-03-27 09:18:45 -0300

    resolvingLogs

---
+++ constants/icons.js
@@ -0,0 +1,29 @@
+import bookmark from "../Front-end/assets/icons/bookmark.png";
+import home from "../Front-end/assets/icons/home.png";
+import plus from "../Front-end/assets/icons/plus.png";
+import profile from "../Front-end/assets/icons/profile.png";
+import leftArrow from "../Front-end/assets/icons/left-arrow.png";
+import menu from "../Front-end/assets/icons/menu.png";
+import search from "../Front-end/assets/icons/search.png";
+import upload from "../Front-end/assets/icons/upload.png";
+import rightArrow from "../Front-end/assets/icons/right-arrow.png";
+import logout from "../Front-end/assets/icons/logout.png";
+import eyeHide from "../Front-end/assets/icons/eye-hide.png";
+import eye from "../Front-end/assets/icons/eye.png";
+import play from "../Front-end/assets/icons/play.png";
+
+export default {
+  play,
+  bookmark,
+  home,
+  plus,
+  profile,
+  leftArrow,
+  menu,
+  search,
+  upload,
+  rightArrow,
+  logout,
+  eyeHide,
+  eye,
+};
